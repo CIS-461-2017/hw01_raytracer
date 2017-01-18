d10a9aff171676e7e76eb876b8df8d52b80827f1
Adam Mally <admally@gmail.com>
2017-01-17 19:19:33 -0500

    Added additional JSON

--- hw01_basecode/src/raytracing/ray.cpp
+++ hw01_basecode/src/raytracing/ray.cpp
@@ -2,8 +2,7 @@
 
 Ray::Ray(const glm::vec3 &o, const glm::vec3 &d):
     origin(o),
-    direction(d),
-    transmitted_color(1,1,1)
+    direction(d)
 {}
 
 Ray::Ray(const glm::vec4 &o, const glm::vec4 &d):
@@ -13,7 +12,6 @@ Ray::Ray(const glm::vec4 &o, const glm::vec4 &d):
 Ray::Ray(const Ray &r):
     Ray(r.origin, r.direction)
 {
-    transmitted_color = r.transmitted_color;
 }
 
 Ray Ray::GetTransformedCopy(const glm::mat4 &T) const
